eef49d4a2f8008c050fdcaa954bae2e33c011dea
Qiwei Liuyang <qiwliu@alu.edu.gva.es>
2026-02-04 12:31:43 +0100

    Codificado lógico del login

--- www/php/login.php
+++ www/php/login.php
@@ -4,4 +4,15 @@
 
     $usuario = $_POST['usuario'] ?? '';
     $contrasena = $_POST['contrasena'] ?? '';
+
+    $usuario_valido = 'admin';
+    $contrasena_valida = 'admin'
+    
+    if($usuario === $usuario_valido && $contrasena === $contrasena_valida){
+        $_SESSION['loggedin'] = true;
+        echo json_encode(['success'=> true, 'message' => 'Login exitoso']);
+    } else {
+        http_response_code(401);
+        echo json_encode(['success'=> false, 'message' => 'Usuario o contraseña incorrectos']);
+    }
 ?>
